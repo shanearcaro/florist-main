52cfe315b3af703acf083aa49e0a7c4e6857689d
Shane Arcaro <smarcaro@gmail.com>
2020-11-20 08:33:31 -0500

    Fixing problem where database uses customer id instead of florist id

--- php/records.php
+++ php/records.php
@@ -10,9 +10,17 @@
         echo "Failed to connect to MYSQL: " . mysql_connect_error();
 
     // Query all results from table
-    $query = "SELECT * FROM OrderRecords INNER JOIN FloristAccounts ON OrderRecords.floristid=FloristAccounts.id 
+
+    // Kinda unfortunate I have to do this but I need to specify florist id separate from customer id because of wrong
+    // values that were being indexed
+    $query = "SELECT florist_firstname AS florist_firstname, florist_lastname AS florist_lastname, FloristAccounts.id AS florist_id, 
+    phone AS phone, email AS email, customer_firstname AS customer_firstname, customer_lastName AS customer_lastname, arrangment AS arrangment, 
+    order_number AS order_number, date AS date, address AS address FROM OrderRecords INNER JOIN FloristAccounts ON OrderRecords.floristid=FloristAccounts.id 
     INNER JOIN CustomerAccounts ON OrderRecords.customerid=CustomerAccounts.id";
     $result = mysqli_query($connection, $query);
+
+    // Adding order number to display otherwise to delete an order you have to have the actual database infront of you
+    // instead of the website's records
 
     
     echo "<div class='container'>";
@@ -21,7 +29,7 @@
     echo "</div>";
     echo "</div>";
 
-    // Create and populate table element
+     // Create and populate table element
 
     echo "<div class=container2>";
     echo "<table id='database_table'>";
@@ -33,17 +41,19 @@
         "</td><th>" . "Customer First Name" .
         "</td><th>" . "Customer Last Name" .
         "</td><th>" . "Items Ordered" .
+        "</td><th>" . "Order Number" .
         "</td><th>" . "Delivery Date" .
         "</td><th>" . "Delivery Address" . "</td></tr>";
     while ($row = mysqli_fetch_array($result)) {
         echo "<tr><td>" . $row['florist_firstname'] . 
             "</td><td>" . $row["florist_lastname"] . 
-            "</td><td>" . $row["id"] . 
+            "</td><td>" . $row["florist_id"] . 
             "</td><td>" . $row["phone"] . 
             "</td><td>" . $row["email"] .
             "</td><td>" . $row["customer_firstname"] . 
             "</td><td>" . $row["customer_lastname"] . 
             "</td><td>" . $row["arrangment"] . 
+            "</td><td>" . $row["order_number"] . 
             "</td><td>" . $row["date"] .
             "</td><td>" . $row["address"] . "</td></tr>";
     }
