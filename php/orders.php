01911ebcdfe529325e4388a05cf7c00d2f3e6b82
Shane Arcaro <smarcaro@gmail.com>
2020-11-19 23:14:16 -0500

    Boiler plate, not done yet

--- php/orders.php
+++ php/orders.php
@@ -1,36 +1,39 @@
-<?php 
-    include 'database.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Flowering Pot</title>
+    <link rel="stylesheet" href="../css/style3.css">
+    <link rel="shortcut icon" href="../favicon.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php
+        include 'index.php';
+    ?>
+    <div class="container" style="width: 25%">
+        <div class="title" id="title">
+            <h1>The Flowering Pot</h1>
+            <h3>Cancel Order</h3>
+        </div>
+        <p style="padding-bottom: 3rem"></p>
+    </div>
 
-    // Connect to SQL database
-    $connection = mysqli_connect($server, $username, $password, $database);
+    <div class="container2" style="width: 25%">
+        <form action="../verify/cancelVerify.php" id="cancelForm" method="post" onsubmit="return confirm('Are you sure you want to delete this record?');">
+            <div class="form_input">
+                <p class="required_text">* Required</p>
+                <input type="text" id="idNumber" name="idNumber" placeholder="Customer ID Number">
 
-    // Check for connection error
-    if (mysqli_connect_error()) 
-        echo "Failed to connect to MYSQL: " . mysql_connect_error();
-    
-    // Query all results from table
-    $query = "SELECT * FROM OrderRecords";
-    $result = mysqli_query($connection, $query);
-
-    // Create and populate table element
-    
-    echo "<table id='database_table'>";
-    echo "<tr><th>" . "Arrangement" .
-        "</td><th>" . "Date" .
-        "</td><th>" . "Address" .
-        "</td><th>" . "Order Number" .
-        "</td><th>" . "Customer ID" .
-        "</td><th>" . "Florist ID" . "</td></tr>";
-    while ($row = mysqli_fetch_array($result)) {
-        echo "<tr><td>" . $row['arrangment'] . 
-            "</td><td>" . $row["date"] . 
-            "</td><td>" . $row["address"] . 
-            "</td><td>" . $row["number"] . 
-            "</td><td>" . $row["customerid"] .
-            "</td><td>" . $row["floristid"] . "</td></tr>";
-    }
-    echo "</table>";
-
-    // End connection to database
-    mysqli_close($connection);
-?>
+                <p class="required_text">* Required</p>
+                <input type="text" id="orderNumber" name="orderNumber" placeholder="Order Number">
+                <div class="buttons" style="padding-left: 7rem">
+                    <button class="buttons" type="submit">Submit</button>
+                </div>
+            </div>
+        </form>
+        <p style="padding-bottom: 3rem"></p>
+    </div>
+</body>
+</html>
